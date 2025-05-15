ac95484b7ab7e69287c84dba56e7b035af070b15
aleorozcol <aorozcolopez@udesa.edu.ar>
2025-05-15 13:29:58 -0300

    ejercicio1 ready

---
+++ ejercicio1/main1.cpp
@@ -0,0 +1,47 @@
+#include "header1.hpp"
+
+int main() {
+    Posicion posicion(-34.6f, -58.4f, 950.0f, 5.3f);
+    Presion presion(101.3f, 5.8f, 6.1f);
+
+    cout << "Datos dados: " << endl;
+    cout << "-Posición: " << endl;
+    posicion.imprimir();
+    cout << "-Presión: " << endl;
+    presion.imprimir();
+    cout << endl;
+
+    SaveFlightData datosVuelo(posicion, presion);
+
+    cout << "Serializando datos en el archivo. " << endl;
+    ofstream outFile("datosVuelo.bin", ios::binary);
+    if (outFile.is_open()) {
+        datosVuelo.serializar(outFile);
+        outFile.close();
+        cout << "Datos guardados correctamente. " << endl;
+    } else {
+        cerr << "Error: no se pudo abrir el archivo. " << endl;
+        return 1;
+    }
+
+    Posicion nuevaPos(0.0f, 0.0f, 0.0f, 0.0f);
+    Presion nuevaPres(0.0f, 0.0f, 0.0f);
+    SaveFlightData nuevosDatos(nuevaPos, nuevaPres);
+    
+    cout << "Deserializando datos desde el archivo. " << endl;
+    ifstream inFile("datosVuelo.bin", ios::binary);
+    if (inFile.is_open()) {
+        nuevosDatos.deserializar(inFile);
+        inFile.close();
+        cout << "Datos cargados correctamente." << endl;
+    } else {
+        cerr << "Error: no se pudo abrir el archivo. " << endl;
+        return 1;
+    }
+
+    cout << endl;
+    cout << "Datos recuperados: " << endl;
+    nuevosDatos.imprimir();
+
+    return 0;
+}
---
+++ ejercicio1/source1.cpp
@@ -0,0 +1,84 @@
+#include "header1.hpp"
+
+//Medicion Base
+
+float MedicionBase::getTiempo() const {
+    if (tiempoMedicion){
+        return *tiempoMedicion;
+    }
+    return 0.0f;
+}
+
+void MedicionBase::serializar(ofstream& out) const {
+    float tiempo = tiempoMedicion ? *tiempoMedicion : 0.0f; 
+    out.write(reinterpret_cast<char*>(&tiempo), sizeof(float));
+} //guardamos el tiempo de medicion en un archivo
+
+void MedicionBase::deserializar(ifstream& in) {
+    float tiempo; // para almacenar el tiempo
+    in.read(reinterpret_cast<char*>(&tiempo), sizeof(float));
+    tiempoMedicion = make_unique<float>(tiempo);
+} //recuperamos el tiempo de medicion desde un archivo
+
+// Presion
+
+void Presion::serializar(ofstream& out) const {
+    MedicionBase::serializar(out);
+    out.write(reinterpret_cast<const char*>(&presionEstatica), sizeof(float));
+    out.write(reinterpret_cast<const char*>(&presionDinamica), sizeof(float));
+}
+
+void Presion::deserializar(ifstream& in) {
+    MedicionBase::deserializar(in);
+    in.read(reinterpret_cast<char*>(&presionEstatica), sizeof(float));
+    in.read(reinterpret_cast<char*>(&presionDinamica), sizeof(float));
+}
+
+void Presion::imprimir() const{
+    cout << "Presion estática: " << presionEstatica << endl;
+    cout << "Presion dinámica: " << presionDinamica << endl;
+    cout << "Tiempo de medición: " << getTiempo() << endl;
+}
+
+// Posicion
+
+void Posicion::serializar(ofstream& out) const {
+    MedicionBase::serializar(out);
+    out.write(reinterpret_cast<const char*>(&latitud), sizeof(float));
+    out.write(reinterpret_cast<const char*>(&longitud), sizeof(float));
+    out.write(reinterpret_cast<const char *>(&altitud), sizeof(float));
+}
+
+void Posicion::deserializar(ifstream& in) {
+    MedicionBase::deserializar(in);
+    in.read(reinterpret_cast<char*>(&latitud), sizeof(float));
+    in.read(reinterpret_cast<char*>(&longitud), sizeof(float));
+    in.read(reinterpret_cast<char *>(&altitud), sizeof(float));
+}
+
+void Posicion::imprimir() const{
+    cout << "Latitud: " << latitud << endl;
+    cout << "Longitud: " << longitud << endl;
+    cout << "Altitud: " << altitud << endl;
+    cout << "Tiempo de medición: " << getTiempo() << endl;
+}
+
+// Save Flight Data
+
+void SaveFlightData::serializar(ofstream& out) const {
+    posicion.serializar(out);
+    presion.serializar(out);
+}
+
+void SaveFlightData::deserializar(ifstream& in) {
+    posicion.deserializar(in);
+    presion.deserializar(in);
+}
+
+void SaveFlightData::imprimir() const{
+    cout << "Datos del vuelo: " << endl;
+    cout << "-Posición: " << endl;
+    posicion.imprimir();
+    cout << "-Presión: " << endl;
+    presion.imprimir();
+}
